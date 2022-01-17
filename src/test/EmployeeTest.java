c4f7043d43f310b1c520c269a0c8f634c59f0fe6
Danae Townsend <danjtown@espol.edu.ec>
2022-01-17 10:55:02 -0500

    Test CsEUREmployee

--- src/test/EmployeeTest.java
+++ src/test/EmployeeTest.java
@@ -6,7 +6,11 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import main.Employee;
+import main.EmployeeType;
 
 public class EmployeeTest {
 
@@ -25,8 +29,29 @@ public class EmployeeTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
 	@Test
+	public void CsEURWorker() {
+		Employee employee = new Employee((float) 3500.0, "EUR", (float) 0.4, EmployeeType.Worker);
+		assertEquals( 3389.333333, employee.cs() , 0.001);
+		
+	}
+	
+	@Test
+	public void CsEURSupervisor() {
+		Employee employee = new Employee((float) 6500.0, "EUR", (float) 0.2, EmployeeType.Supervisor);
+		assertEquals( 6239.403333, employee.cs() , 0.001);
+		
+	}
+	
+	@Test
+	public void CsEURManager() {
+		Employee employee = new Employee( 10000.0f, "EUR",  0.1f, EmployeeType.Manager);
+		assertEquals( 9564.403333, employee.cs() , 0.001);
+		
+	}
+
+	@Ignore
 	public void test() {
 		fail("Not yet implemented");
 	}
